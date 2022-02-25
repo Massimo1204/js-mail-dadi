3fcbc7a1616f7c7d316058f58fb9b45b0310807a
Massimo1204 <xiangmassimo@gmail.com>
2022-02-25 16:57:12 +0100

    versione giusta

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ const userEmail = document.getElementById('my-email');
 const checkResult = document.getElementById('my-result');
 const acceptedEmails = ['mariorossi@gmail.com'];
 
-function checkEmail(email){
-    return email = userEmail.value;
+function keyEnter(e){
+    if(e.keyCode == '13'){
+        console.log(userEmail.value);
+        check();
+    }
 }
-console.log(userEmail.value);
-if (acceptedEmails.find(checkEmail) != undefined){
-    checkResult.classList.remove('d-none');
+function check(){
+    for(let i = 0; i < acceptedEmails.length+1 ; i++ ){
+        if( userEmail.value == acceptedEmails[i]){
+            checkResult.innerHTML="BRAVO!";
+            i=acceptedEmails-length+1;
+        }
+        else{
+            checkResult.innerHTML="NON BRAVO!";
+        }
+    }
 }
-else{
-    checkResult.classList.add('d-none');
-}
+
+userEmail.addEventListener("keypress", keyEnter);
